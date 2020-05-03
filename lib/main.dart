6eac5d7104d1a6550d4d68c3f17b6fbb1ed2d501
givethedog <mung4717@gmail.com>
2020-05-03 21:33:26 +0900

    Splash page - design

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutternaechelinguidestartup/splash_screen.dart';
+
+
 void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'home',
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        body:
-        Center(
-          child: Image.asset('assets/logo.jpg'), //   <-- image
-        )
-
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+        debugShowCheckedModeBanner: false,
+        title: 'home',
+        home: SplashScreen()
+
+
     );
   }
 }
