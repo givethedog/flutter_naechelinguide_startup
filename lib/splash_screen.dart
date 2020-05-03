5fb2b5c2a9219faa1cec2744a351032d1668891a
givethedog <mung4717@gmail.com>
2020-05-03 15:49:41 +0900

    Splash page - design

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:shimmer/shimmer.dart';
+
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -18,56 +17,32 @@ class _SplashScreenState extends State<SplashScreen> {
           mainAxisAlignment: MainAxisAlignment.center ,
           children: <Widget>[
 
-//            Opacity(
-//                opacity: 0.5,
-//                child: Image.asset('assets/images/logo.jpg')
-//            ),
-
             Shimmer.fromColors(
               baseColor: Color(0xffd74b3b),
               highlightColor: Color(0xffefb7b0),
               child: Container(
+                  padding: EdgeInsets.all(20.0),
                   alignment: Alignment.center,
                   child: Column(
                     children: <Widget>[
                       Text(
-                          "NAECHELIN GUIDE",                                            // 어플 이름
+                          "NAECHELIN GUIDE",                                          // 어플 이름
                           style: TextStyle(
-                              fontSize: 40.0,                                           // 글자 크기
-                              fontFamily: 'Franklin',                                   // 글자 폰트
-//                              shadows: <Shadow>[
-//                                Shadow(
-//                                    blurRadius: 10.0,
-//                                    color: Colors.black87,
-//                                    offset: Offset.fromDirection(120, 5)
-//                                )
-//                              ]
+                            fontSize: 40.0,                                           // 글자 크기
+                            fontFamily: 'Franklin',                                   // 글자 폰트
                           )
                       ),
                       Text(
-                          "A BETTER WAY TO FULL",
+                          "A BETTER WAY TO FULL",                                     // 어플 이름
                           style: TextStyle(
-                              fontSize: 20.0,
-                              fontFamily: 'Franklin',
-//                              shadows: <Shadow>[
-//                                Shadow(
-//                                    blurRadius: 10.0,
-//                                    color: Colors.black87,
-//                                    offset: Offset.fromDirection(120, 2)
-//                                )
-//                              ]
-
-                    )
-
+                            fontSize: 20.0,                                           // 글자 크기
+                            fontFamily: 'Franklin',                                   // 글자 폰트
+                          )
+                      )
+                    ],
                   )
-                  ],
-                )
-
               ),
             )
-
-
-
           ],
         ),
       ),
